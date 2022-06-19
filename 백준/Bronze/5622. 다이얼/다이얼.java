f0196103fa93685e7fb24bbd28b15224ddf27e70
Injae Yu <ijyu93@gmail.com>
2022-06-19 23:33:17 +0900

    Update 다이얼.java
    코드 변경

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -3,13 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static final int baseTime = 2;
+    static final int baseAddTime = 1;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputStr = br.readLine();
 
         int resultNum = 0;
         for(char c : inputStr.toCharArray())
-            resultNum += charToNum(c) + 1;
+            resultNum += charToNum(c);
 
         System.out.println(resultNum);
     }
@@ -19,39 +22,39 @@ public class Main {
             case 'A':
             case 'B':
             case 'C':
-                return 2;
+                return baseTime + baseAddTime;
             case 'D':
             case 'E':
             case 'F':
-                return 3;
+                return baseTime + (baseAddTime * 2);
             case 'G':
             case 'H':
             case 'I':
-                return 4;
+                return baseTime + (baseAddTime * 3);
             case 'J':
             case 'K':
             case 'L':
-                return 5;
+                return baseTime + (baseAddTime * 4);
             case 'M':
             case 'N':
             case 'O':
-                return 6;
+                return baseTime + (baseAddTime * 5);
             case 'P':
             case 'Q':
             case 'R':
             case 'S':
-                return 7;
+                return baseTime + (baseAddTime * 6);
             case 'T':
             case 'U':
             case 'V':
-                return 8;
+                return baseTime + (baseAddTime * 7);
             case 'W':
             case 'X':
             case 'Y':
             case 'Z':
-                return 9;
+                return baseTime + (baseAddTime * 8);
             default :
-                return 1;
+                return baseTime;
         }
     }
 }
