3c5510cc9294a5e4b66663a9a5373124092c2a7c
Injae Yu <ijyu93@gmail.com>
2022-07-04 03:40:57 +0900

    Update 멀쩡한 사각형.java
    주석 추가

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -4,6 +4,17 @@ class Solution {
         return ret > 0 ? ret : 0;
     }
     
+    /**
+    유클리드 호제법 구현
+    두 양의 정수 a, b(a > b)에 대하여 a = bq + r(0 <= r < b)
+    r == 0이라면, gcd(a, b) = b가 된다.
+    a = bq1 + r1
+    b = r1q2 + r2
+    r1 = r2q3 + r3
+    ...
+    rn-1 = rnqn+1
+    gcd(a, b) = rn
+    **/
     private static long gcd(long a, long b) {
         if(a < 0 || b < 0) return 0;
         
