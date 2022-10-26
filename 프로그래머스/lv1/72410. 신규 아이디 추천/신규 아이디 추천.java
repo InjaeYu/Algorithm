f1ffa95e5a6b5d609b1f5b4851ce67bea10dde6e
Injae Yu <ijyu93@gmail.com>
2022-10-26 15:26:39 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.42 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase()
+        .replaceAll("[^a-z0-9\\-_\\.]", "")
+        .replaceAll("\\.+", ".")
+        .replaceAll("^\\.", "").replaceAll("\\.$", "");
+    
+        if(answer.length() == 0) {
+            answer = "a";
+        }
+        
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15).replaceAll("\\.$", "");
+        }
+
+        int len = answer.length();
+        if(len < 3) {
+            answer = answer.concat(
+                String.valueOf(
+                    answer.charAt(len - 1))
+                .repeat(3 - len));
+        }
+
+        return answer;
+    }
+}
