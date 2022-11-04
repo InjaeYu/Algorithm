a593ce28bcd57cf9f082c4e5b52047e2ed1a4b05
Injae Yu <ijyu93@gmail.com>
2022-11-04 19:48:49 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.83 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,19 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String s) {
-        String[] sArray = s.split(" ");
-        // int min = Arrays.stream(sArray).mapToInt(Integer::valueOf).min().orElse(0);
-        // int max = Arrays.stream(sArray).mapToInt(Integer::valueOf).max().orElse(0);
-        int min = Integer.valueOf(sArray[0]), max = Integer.valueOf(sArray[0]);
-        for(String o : sArray) {
-            Integer i = Integer.valueOf(o);
-            if(min > i) {
-                min = i;
-            } 
-            if(max < i) {
-                max = i;
-            }
-        }
+        int min = Arrays.stream(s.split(" ")).mapToInt(Integer::valueOf).min().orElse(0);
+        int max = Arrays.stream(s.split(" ")).mapToInt(Integer::valueOf).max().orElse(0);
         String answer = min + " " + max;
         return answer;
     }
