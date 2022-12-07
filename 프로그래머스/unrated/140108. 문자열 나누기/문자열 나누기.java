9cdf2841eedff3a744004d5703533a10db8a4b92
Injae Yu <ijyu93@gmail.com>
2022-12-07 18:00:23 +0900

    [unrated] Title: 문자열 나누기, Time: 0.74 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(String s) {
+        if(s.length() < 3) {
+            return 1;
+        }
+        
+        int answer = 0, baseCount = 1, diffCount = 0;
+        char[] stringArray = s.toCharArray();
+        char base = stringArray[0];
+        for(int i = 1; i < stringArray.length; i++) {
+            if(stringArray[i] == base) {
+                baseCount += 1;
+            } else {
+                diffCount += 1;
+            }
+            
+            if(baseCount == diffCount) {
+                baseCount = 1;
+                diffCount = 0;
+                if(i + 1 < stringArray.length) {
+                    base = stringArray[++i];
+                    answer += 1;
+                }
+            }
+        }
+        return answer + 1;
+    }
+}
