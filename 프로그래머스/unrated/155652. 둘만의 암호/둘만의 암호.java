632bab7dcd207fa8b743162094c8730d5966db8b
Injae Yu <ijyu93@gmail.com>
2023-02-14 17:01:53 +0900

    [unrated] Title: 둘만의 암호, Time: 1.12 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -4,22 +4,21 @@ import java.util.stream.Collectors;
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder ret = new StringBuilder();
-            List<Integer> skipList = skip.chars().boxed().collect(Collectors.toList());
-            for (char c : s.toCharArray()) {
-                int i = 1;
-                while (i <= index) {
-                    c += 1;
-                    if(c > 'z') {
-                        c -= ('z' - 'a') + 1;
-                    }
-                    if(skipList.contains((int)c)) {
-                        continue;
-                    }
-                    i += 1;
+        for (char c : s.toCharArray()) {
+            int i = 1;
+            while (i <= index) {
+                c += 1;
+                if(c > 'z') {
+                    c -= ('z' - 'a') + 1;
                 }
-                ret.append(Character.toChars(c));
+                if(skip.contains(String.valueOf(c))) {
+                    continue;
+                }
+                i += 1;
             }
-    
-            return ret.toString();
+            ret.append(Character.toChars(c));
+        }
+
+        return ret.toString();
     }
 }
